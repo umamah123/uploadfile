309cad3d25eb23e3b18aca540f96c936d3f4961c
umamah123 <91269565+umamah123@users.noreply.github.com>
2021-11-10 16:16:29 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const multer = require("multer");
+const fs = require('fs')
+const app = express();
+
+
+
+
+// Storage Engine That Tells/Configures Multer for where (destination) and how (filename) to save/upload our files
+const fileStorageEngine = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./images"); //important this is a direct path fron our current file to storage location
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "--" + file.originalname);
+  },
+});
+
+
+// The Multer Middleware that is passed to routes that will receive income requests with file data (multipart/formdata)
+// You can create multiple middleware each with a different storage engine config so save different files in different locations on server
+const upload = multer({ storage: fileStorageEngine });
+
+// Single File Route Handler
+app.post("/single", upload.single("image"), (req, res) => {
+  console.log(req.file);
+  res.send("Single FIle upload success");
+});
+
+
+// Multiple Files Route Handler
+app.post("/multiple", upload.array("images", 3), (req, res) => {
+  console.log(req.files);
+  res.send("Multiple Files Upload Success");
+});
+
+app.listen(5000);
